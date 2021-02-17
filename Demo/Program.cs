299ee62a003f22fc93afd808c35972a99950ac5c
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-02-17 09:53:15 -0300

    Initial, example linq.

---
+++ Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 3, 4, 5, 10,11,14 };
+
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
